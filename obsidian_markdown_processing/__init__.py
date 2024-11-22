1b5c6297f4715f1013ea48392adb81b1b1c94596
ryanGT <ryanwkrauss@gmail.com>
2024-11-22 13:31:34 -0500

    random check in

--- obsidian_markdown_processing/__init__.py
+++ obsidian_markdown_processing/__init__.py
@@ -23,6 +23,123 @@ def find_next_folder_num(myroot=".", pat="class_%0.2i_*"):
             return i
 
 
+class obs_figure_syntax_processor(self):
+    """A class to convert a chunk of lines from an obsidian markdown
+    file that contain figure infomation into pandoc/latex code.
+
+    The first line should contain ![[filename]].
+
+    Subsequent lines could be blank or start with :fw:, :fh:, caption: 
+    and label:"""
+    def __init__(self, linesin, fig_folder_path, \
+            default_width='0.9\\textwidth'):
+        self.linesin = copy.copy(linesin)
+        self.default_width = default_width
+        self.fig_folder_path = fig_folder_path
+        if fig_folder_path == 'figs':
+            figfolder = fig_folder_path
+        else:
+            rest, figfolder = os.path.split(fig_folder_path)
+        self.figfolder = figfolder
+
+
+
+    def find_str(self, search_str):
+        found = False
+        for line in self.linesin:
+            if search_str in line:
+                found = True
+
+        return found
+
+
+    def find_line(self, search_str):
+        """return the line containing search_str, if it is found
+        in self.linesin"""
+        for line in self.linesin:
+            if search_str in line:
+                return line
+
+
+    def check_for_pdf(self):
+        fno, ext = os.path.splitext(self.filename)
+        if ext != '.pdf':
+            pdf_name = fno + '.pdf'
+            pdf_path = os.path.join(self.fig_folder_path, pdf_name)
+            if os.path.exists(pdf_path):
+                self.filename = pdf_name
+
+
+    def parse(self):
+        """Go through self.linesin and extract information:
+           - filename
+           - fw
+           - fh
+           - caption
+           label"""
+        q = obs_fig_p.search(self.linesin[0])
+        self.filename = q.group(1)
+        self.check_for_pdf()
+        # how do I handle pdf files?
+        # - if the pdf exists, use it
+        # - does this processor have enough info to know 
+        #   whether or not the pdf exists?
+        attr_map = {':fw:':'fw', \
+                    ':fh:':'fh', \
+                    'caption:':'caption',\
+                    'label:':'label'}
+        for key, attr in attr_map.items():
+            curline = self.find_line(key)
+            if curline is not None:
+                curline = curline.strip()
+                if curline[0] == ':':
+                    curline = curline[1:]
+                rest, mystr = curline.split(':',1)
+                if fw in key:
+                    val_str = mystr + '\\textwidth'
+                elif fh in key:
+                    val_str = mystr + '\\textheight'
+                else:
+                    val_str = mystr
+            else:
+                val_str = None
+
+            setattr(self, attr, val_str)
+
+
+    def convert_to_latex(self):
+        # - do I have :fw: or :fh:?
+        #     - that should affect my latex function
+        # - do I have a caption or label?
+        # - possible outputs:
+        #     - \myfig{}{}
+        #     - \myvfig{}{}
+        #     - \mycapfig{}{}{}
+        # - should one class try to handle all cases?
+        self.parse()
+        relpath = self.figfolder + '/' + self.filename 
+        if self.caption is not None:
+            if self.label is not None:
+                self.caption += ' \\label{%s}' % self.label
+            if self.fw is None:
+                self.fw = self.default_width
+            outline = "\\mycapfig{%s}{%s}{%s}" % \
+                        (self.fw, relpath, self.caption)
+        elif self.fh is not None:
+            outline = "\\myvfig{%s}{%s}" % (self.fh, relpath)
+        else:
+            # defaut is myfig with default_width
+            if self.fw is None:
+                self.fw = self.default_width
+            outline = "\\myfig{%s}{%s}" % (self.fw, relpath)
+
+        N = len(self.linesin)
+        linesout = ['']*N
+        linesout[0] = outline
+        self.linesout = linesout
+        return linesout
+
+
 
 class obsidian_markdown_processor(txt_mixin.txt_file_with_list):
     """The main idea is that this processor will find a markdown
@@ -109,6 +226,88 @@ class obsidian_markdown_processor(txt_mixin.txt_file_with_list):
         
         for figpath in self.fig_obs_paths:
             self.copy_one_fig(figpath, figfolder)
+
+
+    def get_fig_lines(self, ind, lines_to_check=5):
+        """The figure lines start with the line that begins with
+        ![[.  The figure lines may also include lines that start with 
+        :fw:, :fh:, caption:, or label:, along with blank lines below 
+        the first line
+
+        The process will be to check a certain number of lines below
+        the first line for lines that start with an optional string."""
+        first_line = self.list[ind]
+
+        opt_list = [":fw:", ":fh:", "caption:", "label:"]
+
+        for i in range(1, lines_to_check+1):
+            curline = self.list[ind + i]
+            if curline.strip():
+                # the line is not blank
+                # - check to see if it contains any of the optional strings
+                found_one = False
+                for item in opt_list:
+                    if item in curline:
+                        found_one = True
+                        break
+                if not found_one:
+                    # the line was not blank and it contains non-figure
+                    # related content
+                    # - so, we are done and the figure lines 
+                    #   end at the previous line
+                    end_ind = ind + i - 1
+
+        # how do I want to handle blank lines at the end of the 
+        # figure lines?
+        # - backup to the last non-blank line
+        for i in range(lines_to_check):
+            curline = self.list[end_ind - i]
+            if curline.strip():
+                # the line is not blank
+                end_ind -= i
+                break
+        return self.list[ind:end_ind+1]
+
+    
+
+
+    def  process_one_figure(self, ind, figfolder="figs"):
+        """For each figure line that starts with ![[, convert that 
+        line (and possibly subsequent lines) to \myfig{}{}, 
+        \myvfig{}{} or \mycapfig{}{}
+
+        check subsequent lines for :fw:, :fh:, and possibly caption:
+        and label:
+
+        How do I do this in a general way that subclasses can adapt?
+        - is it sufficient that the presence of caption triggers
+          myfigcap?
+
+        :fh: and :fw: might be present in multiple output formats
+        - I want to extact the information in a reusable way
+        - each subclass would then decide what to do with the information
+
+        - does a helper class make sense here?
+        """
+        fig_lines = self.get_fig_lines(ind)
+        figdir1 = os.path.join(self.dst_dir, figfolder)
+        figdir = os.path.abspath(figdir1)
+        myprocessor = obs_figure_syntax_processor(fig_lines, \
+                       fig_folder_path=figdir)
+        myprocessor.parse()
+        outlines = myprocessor.convert_to_latex()
+ 
+
+
+    def process_figure_syntax(self):
+       """Convert obsidian figure syntax to pandoc/latex by 
+       calling process_one_figure for each figure line index."""
+       if not hasattr(self, "fig_inds"):
+            self.get_fig_inds()
+
+        for i in self.fig_inds:
+            self.process_one_figure(i)
+
 
 
 
